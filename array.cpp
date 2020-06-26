d64b685e720a16be633900a21c6b8037212ed075
Gautham-gk <64016915+Gautham-gk@users.noreply.github.com>
2020-06-26 20:26:06 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ int main()
+ {
+int arr[5];
+cout<<"Enter elements:";
+int i;
+ for(i=0;i<5;i++)
+ {
+ 	cin>>arr[i];
+	 }
+	cout<<"You Entered:"; 
+ i=0;		
+ {
+ 	cout<<arr[i]<<endl;
+	 }	
+ }
